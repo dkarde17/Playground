28651f240540c8ba41d0fa95e09514fc3a039a9a
Divye <dkarde17@gmail.com>
2018-03-04 00:44:33 +0530

    Added new module

--- hackerrank/src/main/java/algorithms/implementation/BreakingTheRecords.java
+++ hackerrank/src/main/java/algorithms/implementation/BreakingTheRecords.java
@@ -1,4 +1,42 @@
 package algorithms.implementation;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+/**
+ * https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+ */
 public class BreakingTheRecords {
+    static int[] breakingRecords(int[] score) {
+        // Complete this function
+        int[] recordBreakingRecord = {0,0};
+        int minS, maxS;
+        minS = maxS = score[0];
+        for(int i = 1; i < score.length; i++) {
+            if(score[i] > maxS)
+                recordBreakingRecord[0]++;
+            if(score[i] < minS)
+                recordBreakingRecord[1]++;
+        }
+        return recordBreakingRecord;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] score = new int[n];
+        for(int score_i = 0; score_i < n; score_i++){
+            score[score_i] = in.nextInt();
+        }
+        int[] result = breakingRecords(score);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+        }
+        System.out.println("");
+
+
+        in.close();
+    }
 }
