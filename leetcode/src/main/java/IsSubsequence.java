3354290c7e7f7ae3aa8222f6d5cade0b5d473c38
Divye Karde <dkarde17@gmail.com>
2023-07-18 00:31:06 +0530

    leetcode 392

---
+++ leetcode/src/main/java/IsSubsequence.java
@@ -0,0 +1,49 @@
+/**
+ * leetcode 392 https://leetcode.com/problems/is-subsequence/description/?envType=study-plan-v2&envId=top-interview-150
+ *
+ * Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+ *
+ * A subsequence of a string is a new string that is formed from the original string by deleting some (can be none)
+ * of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a
+ * subsequence of "abcde" while "aec" is not).
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "abc", t = "ahbgdc"
+ * Output: true
+ * Example 2:
+ *
+ * Input: s = "axc", t = "ahbgdc"
+ * Output: false
+ *
+ *
+ * Constraints:
+ *
+ * 0 <= s.length <= 100
+ * 0 <= t.length <= 104
+ * s and t consist only of lowercase English letters.
+ *
+ *
+ * Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, and you want to check one by
+ * one to see if t has its subsequence. In this scenario, how would you change your code?
+ */
+public class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        int ps = 0; int pt = 0;
+        int sl = s.length();
+        int tl = t.length();
+        while(ps < sl && pt < tl) {
+            while(pt < tl && t.charAt(pt) != s.charAt(ps))
+                pt++;
+            if(pt == tl)
+                break;
+            else {
+                pt++;
+                ps++;
+            }
+        }
+        return ps == sl;
+    }
+}
